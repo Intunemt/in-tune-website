42ca22cc2f059fdb55ef0d5821e00759488ae2d5
intunemt <intunemusictherapyllc@gmail.com>
2026-01-20 17:26:35 -0600

    replace static resources with eleventy-generated resources

---
+++ Blog/.eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function () {
+  return {
+    dir: {
+      input: "src",    // where your posts and layouts live
+      output: "_site"  // where Eleventy will generate the HTML
+    }
+  };
+};
---
+++ Blog/Src/first-post.md
@@ -0,0 +1,6 @@
+---
+layout: base.njk
+title: "My First Blog Post"
+date: 2026-01-19
+---
+Hello! This is my very first blog post.
